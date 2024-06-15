0b418efdecaca4f7e7064df904cdbf739eb1b8a4
kakl <kakl@astro.cz>
2024-06-15 15:33:04 +0200

    triggerovaci uP

---
+++ arduino/pipak/pipak.ino
@@ -0,0 +1,98 @@
+/*
+  Pipak do auta
+ 
+ISP
+---
+PD0     RX
+PD1     TX
+RESET#  through 50M capacitor to RST#
+
+SDcard
+------
+DAT3   SS   4 B4
+CMD    MOSI 5 B5
+DAT0   MISO 6 B6
+CLK    SCK  7 B7
+
+ANALOG
+------
++      A0  PA0
+-      A1  PA1
+RESET  0   PB0
+
+LED
+---
+LED_yellow  23  PC7         // LED for Dasa
+
+
+                     Mighty 1284p    
+                      +---\/---+
+           (D 0) PB0 1|        |40 PA0 (AI 0 / D24)
+           (D 1) PB1 2|        |39 PA1 (AI 1 / D25)
+      INT2 (D 2) PB2 3|        |38 PA2 (AI 2 / D26)
+       PWM (D 3) PB3 4|        |37 PA3 (AI 3 / D27)
+    PWM/SS (D 4) PB4 5|        |36 PA4 (AI 4 / D28)
+      MOSI (D 5) PB5 6|        |35 PA5 (AI 5 / D29)
+  PWM/MISO (D 6) PB6 7|        |34 PA6 (AI 6 / D30)
+   PWM/SCK (D 7) PB7 8|        |33 PA7 (AI 7 / D31)
+                 RST 9|        |32 AREF
+                VCC 10|        |31 GND
+                GND 11|        |30 AVCC
+              XTAL2 12|        |29 PC7 (D 23)
+              XTAL1 13|        |28 PC6 (D 22)
+      RX0 (D 8) PD0 14|        |27 PC5 (D 21) TDI
+      TX0 (D 9) PD1 15|        |26 PC4 (D 20) TDO
+RX1/INT0 (D 10) PD2 16|        |25 PC3 (D 19) TMS
+TX1/INT1 (D 11) PD3 17|        |24 PC2 (D 18) TCK
+     PWM (D 12) PD4 18|        |23 PC1 (D 17) SDA
+     PWM (D 13) PD5 19|        |22 PC0 (D 16) SCL
+     PWM (D 14) PD6 20|        |21 PD7 (D 15) PWM
+                      +--------+
+*/
+
+#define RESET       0    // PB0
+#define SDpower1    1    // PB1
+#define SDpower2    2    // PB2
+#define SDpower3    3    // PB3
+#define SS          4    // PB4
+#define MOSI        5    // PB5
+#define MISO        6    // PB6
+#define SCK         7    // PB7
+#define INT         20   // PC4
+#define PIPAK       19   // PC3
+#define TRIGGER     23   // PC7
+#define BUTTON      22   // PC6
+
+
+void setup()
+{
+  pinMode(TRIGGER, INPUT);
+  pinMode(BUTTON, INPUT);
+  pinMode(PIPAK, OUTPUT);
+  digitalWrite(PIPAK, LOW);  
+
+  DDRB = 0xFF;
+  PORTB = 0;     
+}
+
+
+void loop()
+{
+  while(true)
+  {
+    while(!digitalRead(TRIGGER) && !digitalRead(BUTTON));
+
+    PORTB = 0xFF;     
+    
+    for(uint16_t n=0; n<1000; n++)
+    {
+      digitalWrite(PIPAK, HIGH);  
+      delayMicroseconds(150);      
+      digitalWrite(PIPAK, LOW);  
+      delayMicroseconds(50);      
+    }
+    PORTB = 0x0;     
+    delay(400);
+    //while(digitalRead(TRIGGER));
+  }  
+}
